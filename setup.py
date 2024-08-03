831d6efb90dc57e36549e7d500d524c56d4caf90
shadowy-pycoder <shadowy-pycoder@example.com>
2024-08-03 17:00:47 +0300

    Upgraded to v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="py_node_collector",
-    version="0.0.1",
+    version="0.0.2",
     description="python wrapper for https://github.com/shadowy-pycoder/go-node-collector",
     packages=setuptools.find_packages(include=["py_node_collector"]),
     py_modules=["py_node_collector.collector"],
