08ba013a43c3e220cab4c91d576fa6217995b920
shadowy-pycoder <shadowy-pycoder@example.com>
2024-08-05 07:39:48 +0300

    Upgraded to v0.0.3

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 import setuptools
 
 setuptools.setup(
     name="py_node_collector",
-    version="0.0.2",
-    description="python wrapper for https://github.com/shadowy-pycoder/go-node-collector",
+    version="0.0.3",
+    description="Python wrapper for Prometheus Node Collector https://github.com/shadowy-pycoder/go-node-collector",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(include=["py_node_collector"]),
     py_modules=["py_node_collector.collector"],
     package_data={"py_node_collector": ["*.so"]},
@@ -12,4 +16,20 @@ setuptools.setup(
     author_email="shadowy-pycoder@example.com",
     platforms="Linux",
     url="https://github.com/shadowy-pycoder/py-node-collector",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords=[
+        "prometheus",
+        "metrics",
+        "prometheus-collector",
+        "prometheus-node-exporter",
+    ],
 )
